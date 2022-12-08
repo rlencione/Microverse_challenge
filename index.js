a1298816df18c550954cc26574eed99fe7f6d095
rogar <92257405+rlencione@users.noreply.github.com>
2022-12-08 15:47:05 +0000

    function sockMerchant(n, ar)

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-function gradingStudents(grades) {
-let grade = [];
-  for(i = 0; i < grades.length; i++){
-    let soma_1 = (grades[i]) + 1;
-    let soma_2 = (grades[i]) + 2;
-    
-    if(grades[i] < 38){
-      grade.push(grades[i]);
-    } else if(soma_1 % 5 === 0 > 38){
-      grade.push(soma_1);
-    } else if(soma_2 % 5 === 0 > 38){
-      grade.push(soma_2);
-    } else {
-      grades[i] != soma_1 && grades[i] != soma_2;
-      grade.push(grades[i]);
+function sockMerchant(n, ar) {
+ 
+  for(let i = 0; i < n; i++){
+      
     }
   
+  return meia;
 }
-  return grade;
-}
-let resultado = gradingStudents([73, 67, 38, 33]);
-console.log(resultado);
+let meias = sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+console.log(meias);
