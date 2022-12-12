d3c37ff613f4928fad975b31f92c59477b3f1e30
rogar <92257405+rlencione@users.noreply.github.com>
2022-12-12 15:04:10 +0000

    Sales by Match

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function sockMerchant(n, ar) {
- 
-  for(let i = 0; i < n; i++){
-      
-    }
+    let entry = 0;
+    let count = 0;
+    let size = ar.length;
+    for(let i = 0; i < size; i++){
+      entry = (ar[0]);
+      ar.splice(0, 1);
+   if(ar.includes(entry)){
+       delete ar[0];
+    count ++;
+  } else {  
+  }
+  }
+    return count;
   
-  return meia;
+}
+
 }
 let meias = sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
 console.log(meias);
