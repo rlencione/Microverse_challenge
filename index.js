4ace430c07b64dc0fcf1b1300ef9a126ca5ea208
rogar <92257405+rlencione@users.noreply.github.com>
2023-01-05 23:50:46 +0000

    countingValleys_done

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 
-let u = +1;
-let d = -1;
-let msl = 0;
+
+let dw = 0; 
+let valley = 0;
 
 function countingValleys(steps, char){
   for(let i = 0; i < steps; i++){
-  console.log(char[i]);
+    
+  if(char[i] == "D"){
+    dw --;
+  } else {
+    if(dw == -1){
+      valley++;
+    }
+    dw++;
 }
+  }
+  return valley;
 }
-
-let vale = countingValleys(8,"UDDDUDUU");
+let vale = countingValleys(8, "UDDDUDUU");
 console.log(vale);
+
+/*let a = 10;
+let b = 5;
+let c = a - b;
+
+console.log(c);*/
+
+
+
+
+
+
+
+
+
+
+
+
